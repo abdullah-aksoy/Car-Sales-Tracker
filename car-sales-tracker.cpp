41e7d40e565b63a168dce1084a7516c77f28ee72
aaksoyy <69424127+abdullah-aksoy@users.noreply.github.com>
2024-12-06 09:12:43 +0300

    Update car-sales-tracker.cpp

--- car-sales-tracker.cpp
+++ car-sales-tracker.cpp
@@ -1,127 +1,89 @@
 #include <stdio.h>
 
+int main() {
+    float satisTutar, ucret = 0, enCokSatis = 0;
+    int aracKodu, renaSatis = 0, mazdaSatis = 0, hondaSatis = 0, fordSatis = 0;
+    int renaToplam = 0, mazdaToplam = 0, hondaToplam = 0, fordToplam = 0;
+    char m1 = 'r', m2 = 'r';
 
+    printf("Arac kodunu giriniz (car codes): '1-Renault','2-Mazda','3-Honda','4-Ford'\n");
+    scanf("%d", &aracKodu);
 
-int main()
-{
-	
-	float satisTutar,ucret=0,enCokSatis;
-	int aracKodu, renaSatis=0,mazdaSatis=0,hondaSatis=0,fordSatis=0,enCokGelen,renaToplam=0,mazdaToplam=0,hondaToplam=0,fordToplam=0;
-	char m1,m2;
-	
-	printf("Arac kodunu giriniz: '1','2','3','4'\n");
-	scanf("%d",&aracKodu);
-	
-	while(aracKodu!=111){
-	    
-	    switch(aracKodu){
-	        s
-	        case 1:
-	            printf("Renault arac satis tutarini giriniz: ");
-	            scanf("%d",&renaSatis);
-	            renaToplam++;
-	            ucret+=renaSatis;
-	            break;
-	       
-	       case 2:
-	       	    printf("Mazda arac satis tutarini giriniz: ");
-	            scanf("%d",&mazdaSatis);
-	            mazdaToplam++;
-	            ucret+=mazdaSatis;
-	            break;
-	            
-	        case 3:
-	            printf("Honda arac satis tutarini giriniz: ");
-	            scanf("%d",&hondaSatis);
-	            hondaToplam++;
-	            ucret+=hondaSatis;
-	            break;
-	       
-	       case 4:
-	       	    printf("Ford arac satis tutarini giriniz: ");
-	            scanf("%d",&fordSatis);
-	            fordToplam++;
-	            ucret+=fordSatis;
-	            break;
-	            
-	            
-	       default:
-	            printf("hatali islem yaptiniz.\n");
-	            break;
-	        
+    while (aracKodu != 111) {
+        switch (aracKodu) {
+            case 1:
+                printf("Renault arac satis tutarini giriniz (fuel sales amount): ");
+                scanf("%f", &satisTutar);
+                renaSatis += satisTutar;
+                renaToplam++;
+                ucret += satisTutar;
+                break;
 
-	        
-	        
-	        
-	    }
-	    
-	  	    printf("arac kodu giriniz: ");
-	        scanf("%d",&aracKodu);
-	    
-	    
-	}
-	
-	enCokGelen=renaToplam;
-	    m1='r';
-	    
-	    if(enCokGelen<mazdaToplam){
-	        
-	        enCokGelen=mazdaToplam;
-	        m1='m';
-	        
-	        
-	    }
-	    
-	    if(enCokGelen<hondaToplam){
-	        
-	        enCokGelen=hondaToplam;
-	        m1='h';
-	        
-	        
-	    }
+            case 2:
+                printf("Mazda arac satis tutarini giriniz (fuel sales amount): ");
+                scanf("%f", &satisTutar);
+                mazdaSatis += satisTutar;
+                mazdaToplam++;
+                ucret += satisTutar;
+                break;
 
-	    if(enCokGelen<fordToplam){
-	        
-	        enCokGelen=fordToplam;
-	        m1='f';
-	        
-	        
-	    } 
-        
-	    enCokSatis=renaSatis;
-	    m2='r';
-	    
-	 	 if(enCokSatis<mazdaSatis){
-	        
-	        enCokSatis=mazdaSatis;
-	        m2='m';
-	        
-	        
-	    }
-	    
-	    if(enCokSatis<hondaSatis){
-	        
-	        enCokSatis=hondaSatis;
-	        m2='h';
-	        
-	        
-	    }
+            case 3:
+                printf("Honda arac satis tutarini giriniz (fuel sales amount): ");
+                scanf("%f", &satisTutar);
+                hondaSatis += satisTutar;
+                hondaToplam++;
+                ucret += satisTutar;
+                break;
 
-	    if(enCokSatis<fordSatis){
-	        
-	        enCokSatis=fordSatis;
-	        m2='f';
-	        
-	        
-	    }   
-	
-	    printf("en cok gelen arac: %c (%d kez)\n",m1,enCokGelen);
-	    printf("en cok kazandiran arac: %c (%.2f tl ile)\n",m2,enCokSatis);
-	    
-	    
-	    
+            case 4:
+                printf("Ford arac satis tutarini giriniz (fuel sales amount): ");
+                scanf("%f", &satisTutar);
+                fordSatis += satisTutar;
+                fordToplam++;
+                ucret += satisTutar;
+                break;
+
+            default:
+                printf("Hatali islem yaptiniz (Error).\n");
+                break;
+        }
+
+        printf("Arac kodu giriniz (car code): ");
+        scanf("%d", &aracKodu);
+    }
+
+    // Determine the most frequently sold car
+    int enCokGelen = renaToplam;
+    if (enCokGelen < mazdaToplam) {
+        enCokGelen = mazdaToplam;
+        m1 = 'm';
+    }
+    if (enCokGelen < hondaToplam) {
+        enCokGelen = hondaToplam;
+        m1 = 'h';
+    }
+    if (enCokGelen < fordToplam) {
+        enCokGelen = fordToplam;
+        m1 = 'f';
+    }
+
+    // Determine the car that generated the most revenue
+    enCokSatis = renaSatis;
+    if (enCokSatis < mazdaSatis) {
+        enCokSatis = mazdaSatis;
+        m2 = 'm';
+    }
+    if (enCokSatis < hondaSatis) {
+        enCokSatis = hondaSatis;
+        m2 = 'h';
+    }
+    if (enCokSatis < fordSatis) {
+        enCokSatis = fordSatis;
+        m2 = 'f';
+    }
+
+    printf("En cok gelen arac (the most incoming vehicles): %c (%d kez)\n", m1, enCokGelen);
+    printf("En cok kazandiran arac (the most profitable vehicle): %c (%.2f TL ile)\n", m2, enCokSatis);
+
+    return 0;
 }
-
-
-
-
